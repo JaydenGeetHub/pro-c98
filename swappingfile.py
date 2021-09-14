fb4112204b25e4456af7aa4066d5d3c25c94b112
JaydenGeetHub <90315213+JaydenGeetHub@users.noreply.github.com>
2021-09-14 20:05:53 +0530

    Add files via upload

---
+++ swappingfile.py
@@ -0,0 +1,12 @@
+def swappingFile():
+    filename1=input("enteryourfilename")
+    filename2=input("enteryourfilename")
+    with open(filename1,'r')as a:
+        data_a=a.read()
+    with open(filename2,'r')as b:
+        data_b=b.read()
+    with open(filename1,'w')as a:
+        a.write(data_b)
+    with open(filename2,'w')as b:
+        b.write(data_a)
+swappingFile()
